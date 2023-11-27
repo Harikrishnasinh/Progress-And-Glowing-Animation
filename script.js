ebaaacfdfa751469fba8c6ed41ab5f5b352ea8b9
Harikrishnasinh Zala <harikrishnasinh16@gmail.com>
2023-11-27 10:43:18 +0530

    main

---
+++ script.js
@@ -0,0 +1,22 @@
+if (!CSS.supports('animation-timeline: scroll()')) {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to('.content', {
+    scrollTrigger: {
+      trigger: 'body',
+      scrub: 0.5,
+      start: "top top",
+      end: window.innerHeight * 0.4 },
+
+    scale: 1 });
+
+  gsap.to('.fillers path', {
+    scrollTrigger: {
+      trigger: '.content',
+      scrub: 0.5,
+      ease: 'power4.in',
+      start: "top bottom+=20%",
+      end: "bottom bottom-=50%" },
+
+    strokeDashoffset: 0 });
+
+}
